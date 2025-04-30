9a3c5abc6fa058dd01e3f76e6d1add5bd0170999
ahadnawaz585 <ahadnawaz585@gmail.com>
2025-04-30 18:58:06 +0500

    allowing audio stream

--- src/utils/browserManager.ts
+++ src/utils/browserManager.ts
@@ -58,7 +58,6 @@ export default class BrowserManager {
         '--disable-translate',
         '--hide-scrollbars',
         '--metrics-recording-only',
-        '--mute-audio', // Remove if audio output is needed
         '--no-default-browser-check',
         '--disable-hang-monitor',
         '--disable-prompt-on-repost',
